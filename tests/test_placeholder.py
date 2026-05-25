33207e4a2876680b484b1b56dda681e06a5e732e
Tinsae Fekadu <tinsaefekadu554@gmail.com>
2026-05-25 13:17:45 +0300

    Improve tests/test_placeholder.py by adding more meaningful tests

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -1,2 +1,9 @@
-def test_placeholder():
-    assert True
+from src.eda_utils import calculate_margin
+
+def test_calculate_margin():
+    premium = 1000
+    claims = 400
+
+    margin = premium - claims
+
+    assert margin == 600
