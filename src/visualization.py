0263f230890adcabf27fcf2cb0300ea16a6ced6a
Tinsae Fekadu <tinsaefekadu554@gmail.com>
2026-05-25 13:02:50 +0300

    Improve notebook modularization with src imports

---
+++ src/visualization.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def plot_claim_distribution(df):
+    """
+    Plot claim distribution histogram.
+    """
+    df["TotalClaims"].hist(bins=50)
+
+    plt.title("Distribution of Total Claims")
+    plt.xlabel("Claims")
+    plt.ylabel("Frequency")
+
+    plt.show()
